1f0a7916cd8185bec3d62c87ac2145efc9afc2f7
EC2 Default User <chyou726@outlook.jp>
2023-03-15 13:03:20 +0000

    adminのコントローラー・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,52 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :admins
-  root to: 'homes#top'
+  namespace :admins do
+    get 'orders/show'
+  end
+  namespace :admins do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admins do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admins do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+    
+  end
+  namespace :admins do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+  get '/' => 'homes#top'
+  get '/homes/about'
+
+  devise_for :admins
+
+
+  # , controllers: {
+  #  registrations: "admins/registrations",
+  #  sessions: "admins/sessions",
+  #  passwords: "admins/passwords",
+  #  confirmations: "admins/confirmations"
+  # }
+
+
+  devise_for :customers
+
+
+  # , controllers: {
+  #  registrations: "users/registrations",
+  #  sessions: "users/sessions",
+  #  passwords: "users/passwords",
+  #  confirmations: "users/confirmations"
+  # }
+
 end
+
