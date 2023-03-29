91296ced1ecdc1b6a634e9cc39594675fa80009e
EC2 Default User <chyou726@outlook.jp>
2023-03-29 06:27:32 +0000

    配送先登録/一覧作成

--- app/controllers/publics/addresses_controller.rb
+++ app/controllers/publics/addresses_controller.rb
@@ -1,16 +1,40 @@
 class Publics::AddressesController < ApplicationController
   def index
-  end
-
-  def edit
+    @address = Address.new
+    @addresses = Address.page(params[:page])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to publics_addresses_path
+  end
+
+  def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+       flash[:notice] = "You have created address successfully."
+       redirect_to publics_addresses_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to publics_addresses_path
   end
+
+
+private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_21_072824) do
+ActiveRecord::Schema.define(version: 2023_03_29_054202) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2023_03_21_072824) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "addresses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,6 +59,11 @@ ActiveRecord::Schema.define(version: 2023_03_21_072824) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "customer_references", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "customers", force: :cascade do |t|
